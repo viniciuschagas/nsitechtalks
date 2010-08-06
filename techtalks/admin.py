5c5606c18ada502943a70f048354632fe6fdc6d5
Vinícius das Chagas Silva <vinimaster@gmail.com>
2010-08-06 12:00:16 -0300

    Adicionados arquivos esquecidos. :)

---
+++ techtalks/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from models import Edicao, Foto, PalavraChave, Palestra, Palestrante, Video
+
+admin.site.register(Edicao)
+admin.site.register(Foto)
+admin.site.register(PalavraChave)
+admin.site.register(Palestra)
+admin.site.register(Palestrante)
+admin.site.register(Video)
